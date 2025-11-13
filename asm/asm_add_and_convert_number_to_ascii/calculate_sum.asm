4bbebeb88590ee52b86340dcc352c33fa95fdd33
Apprentice Dev <jangtzunghan@gmail.com>
2025-11-13 13:28:12 +0800

    fix makefile and stuffs; halting asm and cpp and makefile/cmake and vim learning for a few days

--- asm/asm_add_and_convert_number_to_ascii/calculate_sum.asm
+++ asm/asm_add_and_convert_number_to_ascii/calculate_sum.asm
@@ -3,21 +3,14 @@ BITS 64
 .data:
 newline db 0x0A
 
-;export our function to the global whatever table (shit gotta reference linkers and loaders book again, whatever man just learn and stop yapping fr)
 global calsum_and_print
 global _start
-global _debugpls
-global _debugoffset
-global _debugoffsetb4
-global _debugoffseta4
-global _wheretf
 extern _func_convert_unsigned
 
 _start:
 push 195
 push 200
 push 3000
-_wheretf:
 call calsum_and_print
 mov rax, 60    
 mov rdi, 0     
@@ -26,24 +19,6 @@ syscall
 
 
 calsum_and_print:
-;push three arguments onto the stack before ebp pls
-;thankyou bro
-;this serves as the testament and oath that I, retard, will quit my status as a lifelong noob and become a low level c++ and asm master
-;I am going to learn bash, vim, cpp, asm, make, cmake and everything about linux kernel 
-;idk why am i even doing this but one day when I look back, I might be smiling with ease and alightment, that this is how far I have been
-;I know my life has been so bad to the point where I feel like getting cut off from the society,
-;But I will do everything in my power to mend everything, with two folds or even 10 folds the effort of normal people, and I am going to start doing it today
-;I am on kaioken mode from this day onwards. Mark my words. Linus torvalds, bill gay, and all the saints
-;This will be the day I liberate myself from the forever tutorial hell and beginner
-;
-;random stranger pls dont look at this my inner dialogue is cringe as hell as always
-;like please pretend you didnt see this already
-;yes this is the main part of my asm program
-;
-;yeah stfu and get back to work
-;okok bro quit pushing on it rn
-
-
 
 push rbp
 mov rbp, rsp
@@ -59,25 +34,17 @@ mov rcx, [rsp+8*3]
 mov rdx, [rsp+8*2]
 
 
-_debugoffset:
 
 mov rbx, 8
 mul rbx, 3
 sub rsp, rbx
 
-;sum the shit up already, quit procrastinating idiot
-;havent you procrastinated enough already
-;get to work bro
-;ok quit bugging me bro
 add rax, rcx
 add rax, rdx
 
-;allocate the string and call conversion method we custom coded without help (cap but i didnt use vibe code at least, as you can see with the poor coding style)
-;allocate 20 bytes string because max you can get with 64 bit integer is 2^64-1, and that translates to almost 20 digits in decimal and we are printing decimal number
 
 
-sub rsp, 20;obtain the start address
-;i think we need 8 bytes because thats as much as one 64 bit integer can hold
+sub rsp, 20
 push rsp ;I
 push rax
 push 20
@@ -86,10 +53,7 @@ call _func_convert_unsigned
 mov rsi, [rsp+8*2]
 mov rdi, 1
 mov rdx, 20
-_debugoffsetb4:
-_debugoffseta4:
 mov rax, 1
-;mov rsi, rsp ;already did it bevore
 syscall
 
 mov rsi, newline
@@ -101,5 +65,4 @@ syscall
 mov rsp,rbp
 
 pop rbp
-_debugpls:
 ret
--- asm/asm_add_and_convert_number_to_ascii/conversion_lib.asm
+++ asm/asm_add_and_convert_number_to_ascii/conversion_lib.asm
@@ -1,7 +1,5 @@
-;assume all 64 bits cuz 32 bits and 16 bits are ohio and i am a noob
-;I am so noob that I am still working on basic x86_64 application after all this time
-;path to os and kernel dev bro, stay viligant
-
+; Conversion library for unsigned and signed integers to ASCII representation
+; Written for 64-bit x86_64 architecture
 
 BITS 64
 
@@ -11,63 +9,46 @@ global _func_convert_unsigned
 
 .text:
 
-;our integer to ascii conversion method (omg pls fix fixed offset and make it a proper function)
-
-;push arguments in stack and then call _func_convert
-;no using registers for arguments for now, and all variables follow the high to low instead of c order
-;yes devilish naming cuz I like unreadability like in gcc, it cool makes you very mysterious and esoteric or niche feeling
-
+; Test main function that wont be executed
 _start:  
-sub rsp, 8
+sub rsp, 8 ;allocate 8 bytes on 
 mov r11, rsp
-;lea r11, [rsp+7]
 push r11 ;give the high address of the string so the procedure writes from hi to lo
 push 67676767
 push 8
 
 call _func_convert_unsigned
-_debugaftercall:
 mov rax, 1
 mov rdi, 1
 mov rsi, [rsp+8*2]
 mov rdx, 8
-_debugbeforesyscall:
 syscall
+
 mov rax, 60
 mov rdi, 0
 syscall
 
-;
-; function: convert signed numbers only
-
-; argument 1: push first, it is the string address you just allocated on the stack, make sure you have allocated
-; enough space for stirng or else it will go out of bound
-
-; argument 2: the nubmer itself that you want to convert to a string
-; 
-; argument 3: size of the string you just allocated on the stack 
-;
-; returns (nothing), if it ever goes out of bound its on you :)
-
-_func_convert_unsigned: ; i aint dealing with no 2's complement oml :sob: not yet the time
+; Integer to ASCII conversion function for unsigned integers
+; Arguments (pushed onto the stack in reverse order):
+;   1. Address of the string buffer to store the result (high address first)
+;   2. The number to convert
+;   3. Size of the string buffer
+; Returns: No return value (result is stored in the provided buffer)
+_func_convert_unsigned: 
 ;prologue
 push rbp
 mov rbp, rsp
-_debugpls:
 
-mov rsi, [rsp+8*3] ; $rsi is the number you want to convert
+; Extract arguments from the stack
+mov rbx, [rsp+8*2] ; Extract the first argument to rbx (buffer address)
+mov rsi, [rsp+8*3] ; Extract the second argument to rsi (the number ot be converted)
+mov rdi, [rsp+8*4]; Extract the third argument to rdi (the size of the buffer (cuz) we write in reverse order, but no out of bounds check)
 mov r11, rsi
-mov rdi, [rsp+8*4]; $rdi is the string (one byte for each power of 10)
-mov rbx, [rsp+8*2]
 add rdi, rbx
 sub rdi, 1
-;deal with the number (keep testing 2 then adding to result)
-
-;bro are you fucking retarded
-;just keep dividing the dividend by 10, then use remainder as dividend you ninger
 mov r10, 10 ;lowest number and keep dividing by 10 to obtain LSB for printing
 
-
+;to be annotated
 while_still_has_flag:
 ;div r11, r10
 mov rax, r11
@@ -86,7 +67,7 @@ add r8, 48
 mov r12, [rdi]
 mov byte [rdi], r8b
 or [rdi], r12
-sub rdi, 1 ; reverse writing order (From high to low like stack), because we essentially write from lsb digits
+sub rdi, 1 
 
 else:
 mov r11, rax 
